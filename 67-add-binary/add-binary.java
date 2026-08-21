abdc54086e8edbae7f87e191ec508ec1e76129cc
Deva200612 <devadharsh006@gmail.com>
2026-08-21 14:17:51 +0530

    Time: 1 ms (99.94%) | Memory: 43.4 MB (84.21%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -4,9 +4,10 @@ class Solution {
         int i=a.length()-1,j=b.length()-1,c=0;
         while(i>=0||j>=0||c>0){
             int x=c;
-            if(i>=0)x+=a.charAt(i--)-48;
-            if(j>=0)x+=b.charAt(j--)-48;
-            s.append(x%2); c=x/2;
+            if(i>=0)x+=a.charAt(i--)-'0';
+            if(j>=0)x+=b.charAt(j--)-'0';
+            s.append(x%2); 
+            c=x/2;
         }
         return s.reverse().toString();
     }
